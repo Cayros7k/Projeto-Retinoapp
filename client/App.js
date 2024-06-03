bf05dbf0f01a0da888642c6eddfc2df3f8aa1ea6
Cayros7k <contato.animous@gmail.com>
2024-06-03 20:10:32 -0300

    Code documentation and some changes

--- client/App.js
+++ client/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image, View, StyleSheet, Text } from 'react-native';
-import { Button, Provider as PaperProvider, Appbar, BottomNavigation } from 'react-native-paper';
+import { Button, Provider as PaperProvider, Appbar, BottomNavigation, Modal, Portal, Provider } from 'react-native-paper';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 
-// Define um componente de função App como o componente principal
 export default function App() {
   return (
     <PaperProvider>
@@ -13,51 +12,37 @@ export default function App() {
   );
 }
 
-// Define um componente de função AppContainer para conter o conteúdo do aplicativo
 function AppContainer() {
-  // Define o estado 'index' e a função 'setIndex' para controlar a navegação
   const [index, setIndex] = useState(0);
-  // Define o estado 'image' e a função 'setImage' para controlar a imagem selecionada
   const [image, setImage] = useState(null);
-  // Define o estado 'result' e a função 'setResult' para controlar o resultado da detecção
   const [result, setResult] = useState(null);
+  const [modalVisible, setModalVisible] = useState(true);
 
-  // Define uma matriz de rotas para a navegação entre telas
   const routes = [
-    // Rota para as instruções de como tirar uma foto
     { key: 'instructions', title: 'Instructions', icon: 'info' },
-    // Rota para a seleção de imagem
     { key: 'imagePicker', title: 'Image Picker', icon: 'image' },
-    // Rota para exibir o resultado da detecção
     { key: 'result', title: 'Result', icon: 'check' },
   ];
 
-  // Define uma função para renderizar o conteúdo com base na rota
   const renderScene = ({ route }) => {
-    // Verifica a chave da rota
     switch (route.key) {
-      // Se a chave da rota for 'instructions'
       case 'instructions':
-        // Renderiza o componente InstructionsScreen
         return <InstructionsScreen setIndex={setIndex} />;
-      // Se a chave da rota for 'imagePicker'
       case 'imagePicker':
-        // Renderiza o componente ImagePickerExample
         return <ImagePickerExample setImage={setImage} setResult={setResult} setIndex={setIndex} />;
-      // Se a chave da rota for 'result'
       case 'result':
-        // Renderiza o componente ResultScreen
         return <ResultScreen image={image} result={result} />;
-      // Caso contrário, retorna nulo
       default:
         return null;
     }
   };
 
+  const hideModal = () => setModalVisible(false);
+
   return (
-    <>
+    <Provider>
       <Appbar.Header style={styles.appbar}>
-        <Appbar.Content title="RBAPP" titleStyle={styles.appbarTitle} />
+        <Appbar.Content title="RetinoAPP" titleStyle={styles.appbarTitle} />
       </Appbar.Header>
       <BottomNavigation
         navigationState={{ index, routes }}
@@ -67,15 +52,20 @@ function AppContainer() {
         activeColor="#ffffff"
         inactiveColor="#a9a9a9"
       />
-    </>
+      <Portal>
+        <Modal visible={modalVisible} onDismiss={hideModal} contentContainerStyle={styles.modalContainer}>
+          <Text style={styles.modalText}>Este aplicativo não substitui o diagnóstico clínico.</Text>
+          <Button mode="contained" onPress={hideModal} style={styles.button} labelStyle={styles.buttonLabel}>
+            Fechar
+          </Button>
+        </Modal>
+      </Portal>
+    </Provider>
   );
 }
 
-// Componente de função para exibir instruções de como tirar uma foto
 function InstructionsScreen({ setIndex }) {
-  // Função para lidar com o botão de continuar
   const handleContinue = () => {
-    // Muda para a tela do seletor de imagens
     setIndex(1);
   };
 
@@ -85,16 +75,13 @@ function InstructionsScreen({ setIndex }) {
         Siga as instruções abaixo para tirar uma foto corretamente:
       </Text>
       <Text style={styles.instructions}>
-        1. Encontre um local bem iluminado.
+        1. Escolha uma foto com uma boa qualidade.
       </Text>
       <Text style={styles.instructions}>
-        2. Mantenha o objeto ou pessoa de interesse no centro da foto.
+        2. Enquadre um dos olhos.
       </Text>
       <Text style={styles.instructions}>
-        3. Evite reflexos ou sombras excessivas.
-      </Text>
-      <Text style={styles.instructions}>
-        4. Aperte o botão para selecionar uma imagem e iniciar o processo de detecção.
+        3. Aviso: Não leve o resultado desta análise como um exame clínicos.
       </Text>
       <Button
         mode="contained"
@@ -108,56 +95,35 @@ function InstructionsScreen({ setIndex }) {
   );
 }
 
-// Componente de função para selecionar uma imagem
 function ImagePickerExample({ setImage, setResult, setIndex }) {
-  // Função para selecionar uma imagem
   const pickImage = async () => {
-    // Chama a função para abrir a biblioteca de imagens
     let result = await ImagePicker.launchImageLibraryAsync({
-      // Tipos de mídia permitidos
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      // Permite a edição da imagem selecionada
       allowsEditing: true,
-      // Proporção de aspecto da imagem
       aspect: [4, 3],
-      // Qualidade da imagem
       quality: 1,
     });
-    
-    // Verifica se a seleção da imagem não foi cancelada
+
     if (!result.canceled) {
-      // Obtém o URI da imagem selecionada
       const imageUri = result.assets[0].uri;
-      // Atualiza o estado da imagem com o URI da imagem selecionada
       setImage(imageUri);
-      
-      // Define os cabeçalhos para a operação de upload
+
       const uploadHeaders = {
-        // Define o tipo de conteúdo para multipart/form-data
         'content-type': 'multipart/form-data',
       };
 
-      // Envia a imagem selecionada para o servidor
       const response = await FileSystem.uploadAsync(
-        "https://d9fe-2804-d45-e08d-9100-592d-2c98-432-4adc.ngrok-free.app/predict",
-        // URI da imagem selecionada
+        "https://8902-189-50-3-114.ngrok-free.app/predict",
         imageUri,
         {
-          // Nome do campo no formulário de upload
           fieldName: "image",
-          // Define o tipo de upload como multipart
           uploadType: FileSystem.FileSystemUploadType.MULTIPART,
-          // Passa os cabeçalhos para a operação de upload
           headers: uploadHeaders,
         }
       );
 
-      // Analisa a resposta do servidor como JSON
       const jsonResponse = JSON.parse(response.body);
-      // Atualiza o estado do resultado com a resposta do servidor
       setResult(jsonResponse);
-
-      // Muda para a tela de resultados
       setIndex(2);
     }
   };
@@ -176,7 +142,6 @@ function ImagePickerExample({ setImage, setResult, setIndex }) {
   );
 }
 
-// Componente de função para exibir o resultado da detecção
 function ResultScreen({ image, result }) {
   return (
     <View style={styles.container}>
@@ -193,7 +158,7 @@ function ResultScreen({ image, result }) {
 
 const styles = StyleSheet.create({
   appbar: {
-    backgroundColor: '#1C1C1C',
+    backgroundColor: '#000',
   },
   appbarTitle: {
     color: '#ffffff',
@@ -205,6 +170,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#f5f5f5',
     padding: 16,
+    flex: 1,
+    flexDirection: 'column'
   },
   instructionsText: {
     fontSize: 20,
@@ -215,10 +182,10 @@ const styles = StyleSheet.create({
   instructions: {
     fontSize: 16,
     marginBottom: 8,
-    textAlign: 'left',
+    textAlign: 'center',
   },
   button: {
-    backgroundColor: '#2F4F4F',
+    backgroundColor: '#4F4F4F',
     marginBottom: 16,
   },
   buttonLabel: {
@@ -239,6 +206,19 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   bottomNavigation: {
-    backgroundColor: '#1C1C1C',
+    backgroundColor: '#000', 
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+    
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 16,
+    textAlign: 'auto'
   },
 });
--- server/svr.py
+++ server/svr.py
@@ -4,19 +4,25 @@ from keras.preprocessing import image
 import numpy as np
 import tensorflow as tf
 
+# Cria uma instância da aplicação Flask.
 app = Flask(__name__)
 
+# Define a rota ("/") que retorna uma mensagem JSON dizendo "Hello from backend".
 @app.route("/")
 def home():
     return {"message": "Hello from backend"}
 
+# Carrega o modelo treinado a partir do diretório especificado.
 model = tf.keras.models.load_model("./model/modelo_treinado.keras")
 
+# Define a altura e largura das imagens que serão processadas.
 img_height = 180
 img_width = 180
 
+# Define o diretório onde as imagens enviadas serão salvas.
 target_img = os.path.join(os.getcwd(), 'images')
 
+# Cria o diretório para salvar as imagens caso ele não exista.
 if not os.path.exists(target_img):
     os.makedirs(target_img)
 
@@ -33,43 +39,55 @@ def read_image(filename):
     x = np.expand_dims(x, axis=0)
     return x
 
+# Função para calcular a softmax das predições, transformando-as em probabilidades.
 def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=1, keepdims=True)
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
+
+    # Se a requisição for POST, verifica se um arquivo foi enviado e se possui uma extensão permitida.
     if request.method == 'POST':
         file = request.files['image']
         if file and allowed_file(file.filename):
             filename = file.filename
             file_path = os.path.join(target_img, filename)
+
+            # Salva o arquivo no diretório especificado.
             file.save(file_path)
 
             # Lê a imagem e faz a predição
             img = read_image(file_path)
             predictions = model.predict(img)
 
+            # Calcula a softmax das predições.
             predictions = softmax(predictions)
 
-            # Obtém o índice da classe com a maior probabilidade
+            # Obtém o índice da classe com a maior probabilidade.
             class_index = np.argmax(predictions, axis=1)[0]
 
-            # Define os nomes das classes
-            class_names = ['retinoblastoma', 'normal']
+            # Define os nomes das classes.
+            class_names = ['Leucocoria', 'Normal']
             predicted_class = class_names[class_index]
 
-             # Obtém a confiança da classe prevista
+             # Obtém a confiança da classe prevista.
             confidence = predictions[0][class_index] * 10
 
+            # Remove a imagem salva no servidor após a predição.
             os.remove(file_path)
+
+            # Retorna a classe prevista, a confiança da predição e o caminho da imagem como uma resposta JSON.
             return jsonify({
                 "predicted_class": predicted_class,
                 "confidence": f"{confidence * 10:.2f}%",
                 "user_image": file_path
             })
+        # Retorna um erro caso o arquivo não possa ser lido ou a extensão não seja permitida.
         else:
             return jsonify({"error": "Unable to read the file. Please check file extension"})
 
+# Inicia a aplicação Flask em modo debug na porta 8000.
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, port=8000)
